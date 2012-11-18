6774472111f748360d42cb1365403c2cb183628e
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-11-18 12:31:50 -0500

    removed black color

--- assets/images.rb
+++ assets/images.rb
@@ -8,6 +8,9 @@ images = ['bub', 'bub_tail', 'bub_walk_tail', 'bub_walk', 'bub_jump', 'bub_jump_
 8.times do |num|
   image = original_image.crop((num * 30) + 1, 69, 16, 17)
   image = image.scale(3)
+  #image = image.opaque('#010000', '#00FF00FF')
+  image = image.transparent('#010000', (Magick::TransparentOpacity-Magick::OpaqueOpacity).abs)
+
   image.write("#{images[num]}.png")
   image = image.flop
   image.write("#{images[num]}_left.png")
