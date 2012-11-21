be56fbc0f84eeed7d8237692c1d03ab7090ad6c3
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-11-21 15:06:50 -0500

    made the walls look like level 1 bubble bobble

--- game_controller.js
+++ game_controller.js
@@ -7,14 +7,35 @@ var GameController = Class.extend({
         this.images = new Images();
         this.context = $('#gameCanvas').get(0).getContext("2d");
 
-        this.walls = [new Wall(100, 200)];
+        this.walls = [];
 
-        // this.walls=[];
-        // for (var i = 0; i < 100; i++) {
-        //     var randomX=Math.floor(Math.random()*811)
-        //     var randomY=Math.floor(Math.random()*611)
-        //     this.walls.push(new Wall(randomX, randomY));
-        // }
+
+        for (var i = 0; i < 2; i++) {
+            for (var k = 0; k < 27; k++)  {
+                if (i == 0)
+                    this.walls.push(new Wall(0, k * 23));
+                else
+                    this.walls.push(new Wall(755, k * 23));
+            }
+        }
+
+        for (var i = 0; i < 18; i++) {
+            for (var k = 0; k < 2; k++)  {
+                if (k == 0)
+                    this.walls.push(new Wall(i * 45, 0));
+                else
+                    this.walls.push(new Wall(i * 45, 577));
+            }
+        }
+
+
+        for (var i = 1; i < 17; i++) {
+            if (i == 2 || i == 3 || i == 14 || i == 15)
+                continue;
+            for (var k = 1; k < 4; k++)  {
+                this.walls.push(new Wall(i * 45, k * 120 + 90));
+            }
+        }
     },
 
     draw: function() {
--- spec/game_controller_spec.js
+++ spec/game_controller_spec.js
@@ -12,12 +12,14 @@ describe("GameController", function() {
     });
 
     it("should be able to move right", sinon.test(function() {
+        gameController.bub.x = 100;
         this.stub(gameController.control, 'isHoldingRight').returns(true);
         gameController.update();
         expect(gameController.bub.x).toBeGreaterThan(100);
     }));
 
     it("should be able to move left", sinon.test(function() {
+        gameController.bub.x = 100;
         this.stub(gameController.control, 'isHoldingLeft').returns(true);
         gameController.update();
         expect(gameController.bub.x).toBeLessThan(100);
@@ -31,41 +33,40 @@ describe("GameController", function() {
     it("should land on a platform while falling", function() {
         for (var i = 0; i < 100; i++)
             gameController.update();
-        expect(gameController.bub.y).toBe(151);
+        expect(gameController.bub.y).toBe(160);
     });
 
     it("should fall at the left boundary", function() {
-        gameController.bub.x = gameController.walls[0].x - gameController.bub.width(gameController.images);
-        for (var i = 0; i < 100; i++)
-            gameController.update();
-        expect(gameController.bub.y).toBe(151);
+        gameController.bub.x = 100;
+        gameController.walls = [new Wall(100 + gameController.bub.width(gameController.images) , gameController.bub.y + gameController.bub.height(gameController.images))]
+        gameController.update();
+        expect(gameController.bub.y).toBe(100);
 
         gameController.bub.x -= 1;
         gameController.update();
-        expect(gameController.bub.y).toBeGreaterThan(151);
+        expect(gameController.bub.y).toBeGreaterThan(100);
     });
 
     it("should fall at the right boundary", function() {
-        gameController.bub.x = gameController.walls[0].x + gameController.walls[0].width(gameController.images);
-        for (var i = 0; i < 100; i++)
-            gameController.update();
-
-        expect(gameController.bub.y).toBe(151);
+        gameController.bub.x = 100;
+        gameController.walls = [new Wall(100 - gameController.bub.width(gameController.images) + 3 , gameController.bub.y + gameController.bub.height(gameController.images))]
+        gameController.update();
+        expect(gameController.bub.y).toBe(100);
 
         gameController.bub.x += 1;
         gameController.update();
-        expect(gameController.bub.y).toBeGreaterThan(151);
+        expect(gameController.bub.y).toBeGreaterThan(100);
     });
 
     it("should be able to jump", sinon.test(function() {
         for (var i = 0; i < 100; i++)
             gameController.update();
-        expect(gameController.bub.y).toBe(151);
+        expect(gameController.bub.y).toBe(160);
 
         this.stub(gameController.control, 'isJumping').returns(true);
         for (var i = 0; i < 100; i++)
             gameController.update();
-        expect(gameController.bub.y).toBeLessThan(151);
+        expect(gameController.bub.y).toBeLessThan(160);
 
     }));
 
