0ad3fd149c498f5ea4c65fbe642930b125bf489b
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-11-21 15:16:24 -0500

    made dvorak z key also work

--- control.js
+++ control.js
@@ -2,7 +2,9 @@ var Control = Class.extend({
     LEFT_KEY: 37,
     RIGHT_KEY: 39,
     Z_KEY: 90,
-
+    DVORAK_Z_KEY: 186,
+    X_KEY: 88,
+    DVORAK_X_KEY: 81,
 
     init: function() {
         this.left = 0;
@@ -17,6 +19,7 @@ var Control = Class.extend({
             case this.LEFT_KEY: this.left = 1; break;
             case this.RIGHT_KEY: this.right = 1; break;
             case this.Z_KEY: this.z = 1; break;
+            case this.DVORAK_Z_KEY: this.z = 1; break;
             }
         }, this));
         $(document).keyup( $.proxy( function(event) {
@@ -24,6 +27,7 @@ var Control = Class.extend({
             case this.LEFT_KEY: this.left = 0; break;
             case this.RIGHT_KEY: this.right = 0; break;
             case this.Z_KEY: this.z = 0; break;
+            case this.DVORAK_Z_KEY: this.z = 0; break;
             }
         }, this));
     },
