ea14a0b14cbc206629041cc29f29d62077143b7c
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-12-19 15:56:18 -0500

    moved a couple more tests out of game_controller into CollisionDetector

--- spec/collision_detector_spec.js
+++ spec/collision_detector_spec.js
@@ -19,6 +19,12 @@ describe("CollisionDetector", function () {
         expect(collisionDetector.isBubStandingOnFloor()).toBeFalsy();
     });
 
+    it("should fall at the left boundary", function () {
+        collisionDetector.walls = [new Wall(0, collisionDetector.player.y + collisionDetector.player.height())];
+        collisionDetector.walls[0].x = 100 - collisionDetector.walls[0].width() - 1;
+        expect(collisionDetector.isBubStandingOnFloor()).toBeFalsy();
+    });
+
     it("cannot run through left wall", sinon.test(function () {
         player.x = 45;
         expect(collisionDetector.noWallToLeft(player)).toBeFalsy();
@@ -30,5 +36,13 @@ describe("CollisionDetector", function () {
         expect(collisionDetector.noWallToRight(player)).toBeFalsy();
         expect(player.x).toBe(754 - player.width());
     }));
+
+    it("should land on a platform and move the player to make sure there are no missing pixels", function () {
+        collisionDetector.walls = [new Wall(100, collisionDetector.player.y + collisionDetector.player.height() - 2)];
+        expect(collisionDetector.player.y).toBe(100);
+        expect(collisionDetector.isBubStandingOnFloor()).toBeTruthy();
+        expect(collisionDetector.player.y).toBe(98);
+        
+    });
 
 });
--- spec/game_controller_spec.js
+++ spec/game_controller_spec.js
@@ -12,35 +12,6 @@ describe("GameController", function () {
         gameInit.destroyCanvas();
     });
 
-    it("should land on a platform while falling", function () {
-        for (var i = 0; i < 100; i++) {
-            gameController.update();
-        }
-        expect(gameController.player.y).toBe(159);
-    });
-
-    it("should fall at the left boundary", function () {
-        gameController.player.x = 100;
-        gameController.walls.push(new Wall(100 + gameController.player.width(), gameController.player.y + gameController.player.height()));
-        gameController.update();
-        expect(gameController.player.y).toBe(100);
-
-        gameController.player.x -= 1;
-        gameController.update();
-        expect(gameController.player.y).toBeGreaterThan(100);
-    });
-
-    it("should fall at the right boundary", function () {
-        gameController.player.x = 100;
-        gameController.walls.push(new Wall(100 - gameController.player.width() + 3, gameController.player.y + gameController.player.height()));
-        gameController.update();
-        expect(gameController.player.y).toBe(100);
-
-        gameController.player.x += 1;
-        gameController.update();
-        expect(gameController.player.y).toBeGreaterThan(100);
-    });
-
     it("should have 3 enemies", sinon.test(function () {
         expect(gameController.enemies.length).toBe(3);
     }));
