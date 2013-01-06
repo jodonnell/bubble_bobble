387c01053226befec8d54da9b71672838aa11d68
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-01-06 14:49:19 -0500

    got the touch events to kind of work

--- control.js
+++ control.js
@@ -55,94 +55,77 @@ var Control = Class.extend({
     },
 
     getKey: function () {
-        $(document).click($.proxy(function () {
-            if (this.right === 1)
-                this.right = 0;
-            else
-                this.right = 1;
-        },this));
+        document.addEventListener('touchstart', $.proxy(function(e) {
+            e.preventDefault();
+            var touch = e.touches[0];
+
+            if (touch.pageX < 100) {
+                this.pressLeft();
+            }
+            else if (touch.pageX >= 100 && touch.pageX < 400) {
+                this.pressRight();
+            }
+            else if (touch.pageX >= 400 && touch.pageX < 850) {
+                this.pressJump();
+            }
+            else if (touch.pageX >= 850) {
+                this.pressBubble();
+            }
+        }, this), false);
+
+        var touchEnd = function(e) {
+            e.preventDefault();
+
+            this.releaseLeft();
+            this.releaseRight();
+            this.releaseJump();
+            this.releaseBubble();
+
+        };
+        document.addEventListener('touchend', $.proxy(touchEnd, this), false);
+        document.addEventListener('touchcancel', $.proxy(touchEnd, this), false);
+
         $(document).keydown($.proxy(function (event) {
             switch (event.which) {
             case this.LEFT_KEY:
-                if (this.left === 0) {
-                    this.socket.emit('moveLeft', {moving: 'keyDown'});
-                }
-
-                this.left = 1;
+                this.pressLeft();
                 break;
             case this.RIGHT_KEY:
-                if (this.right === 0) {
-                    this.socket.emit('moveRight', {moving: 'keyDown'});
-                }
-                this.right = 1;
+                this.pressRight();
                 break;
             case this.Z_KEY:
-                if (this.z === 0) {
-                    this.socket.emit('pressZ', {moving: 'keyDown'});
-                }
-
-                this.z = 1;
+                this.pressJump();
                 break;
             case this.DVORAK_Z_KEY:
-                if (this.z === 0) {
-                    this.socket.emit('pressZ', {moving: 'keyDown'});
-                }
-
-                this.z = 1;
+                this.pressJump();
                 break;
             case this.X_KEY:
-                if (this.x === 0) {
-                    this.socket.emit('pressX', {moving: 'keyDown'});
-                }
-
-                this.x = 1;
+                this.pressBubble();
                 break;
             case this.DVORAK_X_KEY:
-                if (this.x === 0) {
-                    this.socket.emit('pressX', {moving: 'keyDown'});
-                }
-
-                this.x = 1;
+                this.pressBubble();
                 break;
             }
         }, this));
         $(document).keyup($.proxy(function (event) {
             switch (event.which) {
             case this.LEFT_KEY:
-                if (this.left === 1) {
-                    this.socket.emit('moveLeft', {moving: 'keyUp'});
-                }
-                this.left = 0;
+                this.releaseLeft();
                 break;
             case this.RIGHT_KEY:
-                if (this.right === 1) {
-                    this.socket.emit('moveRight', {moving: 'keyUp'});
-                }
-                this.right = 0;
+                this.releaseRight();
                 break;
             case this.Z_KEY:
-                if (this.z === 1) {
-                    this.socket.emit('pressZ', {moving: 'keyUp'});
-                }
-                this.z = 0;
+                this.releaseJump();
                 break;
             case this.DVORAK_Z_KEY:
-                if (this.z === 1) {
-                    this.socket.emit('pressZ', {moving: 'keyUp'});
-                }
-                this.z = 0;
+                this.releaseJump();
                 break;
             case this.X_KEY:
-                if (this.x === 1) {
-                    this.socket.emit('pressX', {moving: 'keyUp'});
-                }
-                this.x = 0;
+                this.releaseBubble();
                 break;
             case this.DVORAK_X_KEY:
-                if (this.x === 1) {
-                    this.socket.emit('pressX', {moving: 'keyUp'});
-                }
-                this.x = 0;
+                this.releaseBubble();
                 break;
             }
         }, this));
@@ -162,6 +145,63 @@ var Control = Class.extend({
 
     isShooting: function () {
         return this.x;
+    },
+
+    pressLeft: function () {
+        if (this.left === 0) {
+            this.socket.emit('moveLeft', {moving: 'keyDown'});
+        }
+        this.left = 1;
+    },
+
+    pressRight: function () {
+        if (this.right === 0) {
+            this.socket.emit('moveRight', {moving: 'keyDown'});
+        }
+        this.right = 1;
+    },
+
+    pressJump: function () {
+        if (this.z === 0) {
+            this.socket.emit('pressZ', {moving: 'keyDown'});
+        }
+
+        this.z = 1;
+    },
+
+    pressBubble: function () {
+        if (this.x === 0) {
+            this.socket.emit('pressX', {moving: 'keyDown'});
+        }
+        this.x = 1;
+    },
+
+    releaseLeft: function () {
+        if (this.left === 1) {
+            this.socket.emit('moveLeft', {moving: 'keyUp'});
+        }
+        this.left = 0;
+    },
+
+    releaseRight: function () {
+        if (this.right === 1) {
+            this.socket.emit('moveRight', {moving: 'keyUp'});
+        }
+        this.right = 0;
+    },
+
+    releaseJump: function () {
+        if (this.z === 1) {
+            this.socket.emit('pressZ', {moving: 'keyUp'});
+        }
+        this.z = 0;
+    },
+
+    releaseBubble: function () {
+        if (this.x === 1) {
+            this.socket.emit('pressX', {moving: 'keyUp'});
+        }
+        this.x = 0;
     },
 
     notHoldingRightOrLeft: function () {
