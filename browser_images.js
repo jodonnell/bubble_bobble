420d46cd6e893d5ebf27a4a3397a336d9f7bdb7e
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-01-09 19:57:35 -0500

    got the tests running again

--- browser_images.js
+++ browser_images.js
@@ -2,7 +2,12 @@
 
 var BrowserImages = Images.extend({
     _loadImage: function (prop, imageFile) {
+        this._props.push(prop);
+        this[prop] = {ready: false};
+
         this[prop] = new Image();
         this[prop].src = "assets/" + imageFile;
+        if (prop === 'bubRight')
+            this[prop].onload = this._callback;
     }
 });
