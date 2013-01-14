aa552b72c799508edebc9e60d9a528f718a5c72c
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-01-14 02:30:31 -0500

    center the game in the browser viewport and make the background black

--- game_init.js
+++ game_init.js
@@ -12,11 +12,14 @@ var GameInit = Class.extend({
         this.width = 800;
         this.height = 600;
 
+        var left = $(window).width() / 2 - this.width / 2;
+        var top = $(window).height() / 2 - this.height / 2;
+
         var canvas = '<canvas id="gameCanvas" width="' + this.width + '" height="' + this.height + '"></canvas>';
         $('body').append(canvas);
         $("#gameCanvas").css('position', 'absolute');
-        $("#gameCanvas").css('top', '0px');
-        $("#gameCanvas").css('left', '0px');
+        $("#gameCanvas").css('top', top + 'px');
+        $("#gameCanvas").css('left', left + 'px');
         // $("#gameCanvas").css('width', '1066px');
         // $("#gameCanvas").css('height', '800px');
 
