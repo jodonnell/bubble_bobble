a8eb7cda2da769b2a69b2c08a83486acdd9e95cc
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-01-14 02:15:41 -0500

    made the selection of single player or online work correctly

--- title_screen.js
+++ title_screen.js
@@ -14,7 +14,7 @@ var TitleScreen = Class.extend({
         gameContext.fillRect(0, 0, gameInit.width, gameInit.height);
     },
 
-    update: function() {
+    update: function(cancelId) {
         this._timer++;
         if (this._timer === 12) {
             this._timer = 0;
@@ -40,6 +40,65 @@ var TitleScreen = Class.extend({
 
         if (this._holdingUp && !this._control.isHoldingUp()) {
             this._holdingUp = false;
+        }
+
+        if ((this._control.isShooting() || this._control.isJumping()) && this._selectedEntry === 0) {
+            var bub = new Player(200, 100, 'bub', new InputControl(null));
+            var gameController = new GameController(gameInit, [bub]);
+            cancelAnimationFrame(cancelId);
+
+            (function animloop(){
+              stats.begin();
+      
+              gameController.update();
+              gameController.draw();
+              requestAnimFrame(animloop);
+
+              stats.end();
+          })();
+        }
+        else if ((this._control.isShooting() || this._control.isJumping()) && this._selectedEntry === 1) {
+            cancelAnimationFrame(cancelId);
+            var socket = io.connect('http://192.168.0.105:3000');
+
+            socket.on('gameStarted', function(data){
+                if (data.playerNum == 1) {
+                    var bub = new Player(200, 100, 'bub', new InputControl(socket));
+                    var bob = new Player(600, 100, 'bob', new NetworkedControl(socket));
+                }
+                else {
+                    var bub = new Player(200, 100, 'bub', new NetworkedControl(socket));
+                    var bob = new Player(600, 100, 'bob', new InputControl(socket));
+                }
+
+
+                var gameController = new GameController(gameInit, [bub, bob]);
+
+                var oldPositions = {bub: {x: 200, y: 100}, bob: {x: 600, y: 100}};
+
+                socket.on('updatedPositions', $.proxy(function (data) {
+                    
+                    oldPositions = data;
+
+                    gameController.onscreenSprites.players[0].x = data.bub.x;
+                    gameController.onscreenSprites.players[0].y = data.bub.y;
+
+                    gameController.onscreenSprites.players[1].x = data.bob.x;
+                    gameController.onscreenSprites.players[1].y = data.bob.y;
+
+                }));
+
+                (function animloop(){
+                    stats.begin();
+                    
+                    gameController.update();
+                    gameController.draw();
+                    requestAnimFrame(animloop);
+
+                    stats.end();
+                })();
+
+            });
         }
 
     },
