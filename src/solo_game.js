99f63c7815f0335f878c716e483d39d4c3717550
Jacob O'Donnell <jacobodonnell@gmail.com>
2017-03-25 19:57:46 -0400

    var -> const

--- src/solo_game.js
+++ src/solo_game.js
@@ -2,10 +2,10 @@
 
 class SoloGame {
     constructor() {
-        var bub = new Player(200, 100, 'bub', new InputControl(null));
-        var enemies = [new BlueMagoo(1, 370, 20, LEFT), new BlueMagoo(2, 370, 70, LEFT), new BlueMagoo(3, 370, 120, LEFT)];
+        const bub = new Player(200, 100, 'bub', new InputControl(null));
+        const enemies = [new BlueMagoo(1, 370, 20, LEFT), new BlueMagoo(2, 370, 70, LEFT), new BlueMagoo(3, 370, 120, LEFT)];
 
-        var gameController = new GameController(gameInit, [bub], enemies);
+        const gameController = new GameController(gameInit, [bub], enemies);
 
         SceneCreator.create(gameController);
     }
