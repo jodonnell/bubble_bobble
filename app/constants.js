7b449a95505b1bd2fe7489967f08a249aa056d1b
Jacob O'Donnell <jacobodonnell@gmail.com>
2017-04-29 17:54:28 -0400

    adding bundle

--- app/constants.js
+++
@@ -1,7 +0,0 @@
-const RIGHT = 1;
-const LEFT = 2;
-
-const RIGHT_BOUND = 754;
-const LEFT_BOUND = 46;
-
-export {LEFT, RIGHT, LEFT_BOUND, RIGHT_BOUND};
--- spec/game_init_spec.js
+++
@@ -1,5 +0,0 @@
-describe('GameInit', function () {
-    it('should have a canvas element', function () {
-        expect(document.querySelectorAll('#gameCanvas').length).toEqual(1);
-    });
-});
